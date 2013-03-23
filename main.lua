2cf2274b06dc04b2d54f4896ca14f2c4419e5610
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2013-03-23 15:03:22 +0100

    Separate player class code from main.lua :)

--- main.lua
+++ main.lua
@@ -1,93 +1,25 @@
 anim8 = require 'libs/anim8'
+require 'player'
+
+GRAVITY = 400
 
 function love.load()
-   love.graphics.setBackgroundColor(34, 69, 103)
-
-   player = {
-     x = 50,
-     y = 400,
-     x_speed = 200,
-     y_speed = 0,
-     jump_height = 300,
-     direction = 2
-   }
-
-   gravity = 400
-
-   loadAnimations()
+  love.graphics.setBackgroundColor(34, 69, 103)
+  player = Player.create()
 end
 
 function love.update(dt)
-  updatePlayerPosition(dt)
-  updateAnimations(dt)
+  player:update(dt)
 end
 
 function love.draw()
-  drawAnimations()
+  player:draw()
   love.graphics.print("player.x: " .. player.x, 10, 10)
   love.graphics.print("player.y: " .. player.y, 10, 20)
 end
 
 function love.keypressed(key)
   if key == "w" then
-    if player.y_speed == 0 then -- we're probably on the ground, let's jump
-      player.y_speed = player.jump_height
-    end
-  end
-end
-
-function loadAnimations()
-   image_walk_left = love.graphics.newImage("textures/bunny_anim_left.png")
-   player_walk_left_grid = anim8.newGrid(50, 80, image_walk_left:getWidth(), image_walk_left:getHeight())
-   player_walk_left_anim = anim8.newAnimation('loop', player_walk_left_grid('1-2,1'), 0.1)
-
-   image_walk_right = love.graphics.newImage("textures/bunny_anim_right.png")
-   player_walk_right_grid = anim8.newGrid(50, 80, image_walk_right:getWidth(), image_walk_right:getHeight())
-   player_walk_right_anim = anim8.newAnimation('loop', player_walk_right_grid('1-2,1'), 0.1)
-end
-
-function updatePlayerPosition(dt)
-  if love.keyboard.isDown('a') then
-    player.x = player.x - dt * player.x_speed
-  elseif love.keyboard.isDown('d') then
-    player.x = player.x + dt * player.x_speed
-  end
-  if player.y_speed ~= 0 then
-    player.y = player.y - player.y_speed * dt
-    player.y_speed = player.y_speed - gravity * dt
-    if player.y > 400 then
-      player.y_speed = 0
-      player.y = 400
-    end
-  end
-end
-
-function updateAnimations(dt)
-  if player.direction == 1 then
-    player_walk_left_anim:update(dt)
-  elseif player.direction == 2 then
-    player_walk_right_anim:update(dt)
-  end
-end
-
-function drawAnimations()
-  drawPlayer()
-end
-
-function drawPlayer()
-  if love.keyboard.isDown('a') then
-    player_walk_left_anim:draw(image_walk_left, player.x, player.y)
-    player.direction = 1
-  elseif love.keyboard.isDown('d') then
-    player_walk_right_anim:draw(image_walk_right, player.x, player.y)
-    player.direction = 2
-  else
-    if player.direction == 1 then
-      player_walk_left_anim:gotoFrame(2)
-      player_walk_left_anim:draw(image_walk_left, player.x, player.y)
-    elseif player.direction == 2 then
-      player_walk_right_anim:gotoFrame(1)
-      player_walk_right_anim:draw(image_walk_right, player.x, player.y)
-    end
+    player:jump()
   end
 end
--- player.lua
+++ player.lua
@@ -1,19 +1,78 @@
+Player = {}
+Player.__index = Player
 
+function Player.create()
+  local self = {}
+  setmetatable(self, Player)
+  self:initialize()
+  return self
+end
 
-function drawPlayer()
+function Player:initialize()
+  self.x = 50
+  self.y = 400
+  self.x_speed = 200
+  self.y_speed = 0
+  self.direction = 2
+  self.jump_height = 200
+  self:loadAnimations()
+end
+
+function Player:loadAnimations()
+   self.image_walk_left = love.graphics.newImage("textures/bunny_anim_left.png")
+   local walk_left_grid = anim8.newGrid(50, 80, self.image_walk_left:getWidth(), self.image_walk_left:getHeight())
+   self.walk_left_anim = anim8.newAnimation('loop', walk_left_grid('1-2,1'), 0.1)
+
+   self.image_walk_right = love.graphics.newImage("textures/bunny_anim_right.png")
+   local walk_right_grid = anim8.newGrid(50, 80, self.image_walk_right:getWidth(), self.image_walk_right:getHeight())
+   self.walk_right_anim = anim8.newAnimation('loop', walk_right_grid('1-2,1'), 0.1)
+end
+
+function Player:update(dt)
   if love.keyboard.isDown('a') then
-    player_walk_left_anim:draw(image_walk_left, player.x, player.y)
-    player.direction = 1
+    self.x = self.x - dt * self.x_speed
   elseif love.keyboard.isDown('d') then
-    player_walk_right_anim:draw(image_walk_right, player.x, player.y)
-    player.direction = 2
-  else
-    if player.direction == 1 then
-      animation1:gotoFrame(2)
-      animation1:draw(imageR, player.x, player.y)
-    elseif player.direction == 2 then
-      animation2:gotoFrame(1)
-      animation2:draw(imageL, player.x, player.y)
+    self.x = self.x + dt * self.x_speed
+  end
+  if self.y_speed ~= 0 then
+    self.y = self.y - self.y_speed * dt
+    self.y_speed = self.y_speed - GRAVITY * dt
+    if self.y > 400 then
+      self.y_speed = 0
+      self.y = 400
     end
+  end
+  self:updateAnimations(dt)
+end
+
+function Player:updateAnimations(dt)
+  if self.direction == 1 then
+    self.walk_left_anim:update(dt)
+  elseif self.direction == 2 then
+    self.walk_right_anim:update(dt)
+  end
+end
+
+function Player:draw()
+  if love.keyboard.isDown('a') then
+    self.walk_left_anim:draw(self.image_walk_left, self.x, self.y)
+    self.direction = 1
+  elseif love.keyboard.isDown('d') then
+    self.walk_right_anim:draw(self.image_walk_right, self.x, self.y)
+    self.direction = 2
+  else
+    if self.direction == 1 then
+      self.walk_left_anim:gotoFrame(2)
+      self.walk_left_anim:draw(self.image_walk_left, self.x, self.y)
+    elseif self.direction == 2 then
+      self.walk_right_anim:gotoFrame(1)
+      self.walk_right_anim:draw(self.image_walk_right, self.x, self.y)
+    end
+  end
+end
+
+function Player:jump()
+  if self.y_speed == 0 then -- we're probably on the ground, let's jump
+    self.y_speed = self.jump_height
   end
 end
